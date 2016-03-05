8716c2bfd28945a96133a6ed9c364570fba845fc
YanqiangWang <qingtianguzhu@gmail.com>
2016-03-05 20:45:19 +0800

    update

---
+++ Chapter5/E5-23.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdexcept>
+using namespace std;
+
+int main() {
+	int num1, num2;
+	cin >> num1 >> num2;
+	try {
+		if (num2 == 0)
+		 throw runtime_error("error!");
+	cout << num1 / num2;
+	} catch (runtime_error err) {
+		cout << err.what() << endl;
+	}
+	return 0;
+}
