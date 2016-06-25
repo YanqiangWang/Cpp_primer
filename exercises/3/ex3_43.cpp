3db268b116e1c90ff32a6cd8d1dec4c961309ce7
YanqiangWang <qingtianguzhu@gmail.com>
2016-06-25 12:36:29 +0800

    github

---
+++ exercises/3/ex3_43.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstddef>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+	constexpr size_t rowCnt = 3, colCnt = 4;
+	int ia[rowCnt][colCnt];
+	for (size_t i = 0; i != rowCnt; ++i)
+		for (size_t j = 0; j != colCnt; ++j)
+			ia[i][j] = i * colCnt + j;
+		
+	// version 1
+	for (const int (&row)[4] : ia) {
+		for (int col : row) {
+			cout << col << " ";
+		}
+		cout << endl;
+	}
+	
+	// version 2
+	for (int i = 0; i < rowCnt; ++i) {
+		for (int j = 0; j < colCnt; ++j)
+			cout << ia[i][j] << " ";
+		cout << endl;
+	}
+	
+	// version 3
+	for (int (*row)[4] = ia; row != ia + 3; ++row) {
+		for (int *col = *row; col != *row + 4; ++col)
+			cout << *col << " ";
+		cout << endl;
+	}
+	
+	return 0;
+}
